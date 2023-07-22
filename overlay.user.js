1b2dfc23f9facf384e98eb643ffe309146aaa573
Pukima <pukima@pukima.site>
2023-07-23 01:14:23 +0200

    bump version to 1.0.1

--- overlay.user.js
+++ overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/place overlays
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  try to take over the canvas!
 // @author       Pukima
 // @match        https://garlic-bread.reddit.com/embed*
