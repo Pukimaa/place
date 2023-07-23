00e457c371645930611758b8b4c66a7590d0345b
Pukima <pukima@pukima.site>
2023-07-23 02:01:03 +0200

    Add toggle button

--- overlay.user.js
+++ overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/place overlays
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.1.0
 // @description  try to take over the canvas!
 // @author       Pukima
 // @match        https://garlic-bread.reddit.com/embed*
@@ -12,6 +12,7 @@
 
 var overlayImage = null;
 var placedeImage = null;
+var overlayButton = null;
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
         const canvasContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("div.layout").querySelector("garlic-bread-canvas").shadowRoot.querySelector("div.container");
@@ -22,12 +23,30 @@ if (window.top !== window.self) {
         placedeImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1500px;pointerEvents: 'none';`;
         canvasContainer.appendChild(overlayImage);
         canvasContainer.appendChild(placedeImage);
+
+        overlayButton = document.createElement("button");
+        overlayButton.style = "position: absolute; top: 28px;left: 80px;border-radius: 0;border: 3px solid black;padding: 4px 10px;height: fit-content;";
+        overlayButton.textContent = "Overlay: on";
+        overlayButton.addEventListener('click', () => {toggleOverlay();});
+        window.document.body.append( overlayButton );
     }, false);
 }
 
 function updateImage() {
     overlayImage.src = "https://place.kayo.zip/outputs/overlay_target.png?" + Date.now()
     placedeImage.src = "https://place.army/overlay_target.png?" + Date.now()
+}
+
+function toggleOverlay() {
+    if( overlayImage.style.display == "none" ) {
+        overlayImage.style.display = "block";
+        placedeImage.style.display = "block";
+        overlayButton.textContent = "Overlay: on";
+    } else {
+        overlayImage.style.display = "none";
+        placedeImage.style.display = "none";
+        overlayButton.textContent = "Overlay: off";
+    }
 }
 
 setInterval(function () {overlayImage.src = "https://place.kayo.zip/outputs/overlay_target.png?" + Date.now()}, 30000);
